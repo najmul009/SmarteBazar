aa205c6d68753f122a6f700aab0ac62376521307
najmul009 <najmul.tk16@gmail.com>
2022-03-01 00:06:35 +0600

    fetch result show on UI

--- js/app.js
+++ js/app.js
@@ -1,5 +1,26 @@
+//Search btn event handelaar function
 const searchPhone = () => {
     fetch('https://openapi.programming-hero.com/api/phones?search=iphone')
     .then(res => res.json())
-    .then(data => console.log(data))
+    .then(data => displayPhones(data.data))
+}
+//function for fetch result desplay on UI 
+const displayPhones = (phones) =>{
+    // console.log(phones)
+    phones.forEach(phone => {
+        // console.log(phone)
+        const main = document.getElementById('main')
+        const div = document.createElement('div')
+        div.classList.add('col-12', 'col-md-6', 'col-lg-4', 'p-2')
+        div.innerHTML = `
+        <div class="card  p-4">
+            <img src="${phone.image}" class="card-img-top w-100" alt="...">
+            <div class="card-body ">
+                <h5 class="card-title ">${phone.phone_name}</h5>
+                <p class="card-text ">Brand: ${phone.brand}</p>
+            </div>
+        </div>
+        `;
+        main.appendChild(div)
+    });
 }
