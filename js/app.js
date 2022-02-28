df364ffa1f8d8c17f0a9b4d175b4cc991fd6af47
najmul009 <najmul.tk16@gmail.com>
2022-03-01 01:03:49 +0600

    empty str error massage added

--- js/app.js
+++ js/app.js
@@ -1,18 +1,37 @@
+//error function
+const error = (error, errorMsg) => {
+    const Warning = document.getElementById('Warning')
+    const erroeMassege = document.getElementById('erroeMassege')
+    erroeMassege.innerText = errorMsg
+    Warning.style.display = error
+}
+const closeError = () => {
+    error('none')
+}
+// document.getElementById('closeWarning').addEventListener('click',function(){
+//     error('none')
+// };
 //Search btn event handelaar function
 const searchPhone = () => {
     const main = document.getElementById('main')
-    main.innerHTML=''
-    const searchValue = document.getElementById('search-field').value ;
+    main.innerHTML = ''
+    const searchValue = document.getElementById('search-field').value;
     const searchStr = searchValue.toString()
     const searchText = searchStr.toLowerCase()
     // console.log(searchText)
-    const url = `https://openapi.programming-hero.com/api/phones?search=${searchText}`
-    fetch(url)
-    .then(res => res.json())
-    .then(data => displayPhones(data.data))
+    if (searchStr == '') {
+        error('block', '!please search by your mobile name')
+    } else {
+        const url = `https://openapi.programming-hero.com/api/phones?search=${searchText}`
+        fetch(url)
+            .then(res => res.json())
+            .then(data => displayPhones(data.data))
+        
+            error('none')
+    }
 }
 //function for fetch result desplay on UI 
-const displayPhones = (phones) =>{
+const displayPhones = (phones) => {
     // console.log(phones)
     phones.forEach(phone => {
         // console.log(phone)
